c2fd6f6c2f5cd6587f38a7ed6dc535c8278f334d
Vương Nguyễn Đại <24522050@ms.uit.edu.vn>
2026-03-14 19:02:47 +0700

    feat: Hoan thien logic chon phien ban RAM

--- app/src/main/java/com/example/itstore/ProductDetailActivity.java
+++ app/src/main/java/com/example/itstore/ProductDetailActivity.java
@@ -33,12 +33,8 @@ public class ProductDetailActivity extends AppCompatActivity {
         android.widget.TextView tvOldPrice = findViewById(R.id.tvPriceOld);
         tvOldPrice.setPaintFlags(tvOldPrice.getPaintFlags() | android.graphics.Paint.STRIKE_THRU_TEXT_FLAG);
         tvName.setText(currentProduct.getName());
-        tvPrice.setText(String.format(java.util.Locale.US, "%,.0f VNĐ", currentProduct.getPrice()));
         android.view.View btnBack = findViewById(R.id.ivBack);
-        double oldPrice = currentProduct.getCompareAtPrice();
-        java.text.DecimalFormat formatter = new java.text.DecimalFormat("#,###");
-        String formattedOldPrice = formatter.format(oldPrice) + " VNĐ";
-        tvOldPrice.setText(formattedOldPrice);
+        updatePriceDisplay(currentProduct.getPrice(), currentProduct.getCompareAtPrice());
         java.util.List<Integer> listImages = new java.util.ArrayList<>();
         listImages.add(R.drawable.ram1);
         ImagePagerAdapter imageAdapter = new ImagePagerAdapter(listImages);
@@ -49,5 +45,31 @@ public class ProductDetailActivity extends AppCompatActivity {
                 finish();
             }
         });
+        android.widget.TextView cardChoice1 = findViewById(R.id.cardChoice1);
+        android.widget.TextView cardChoice2 = findViewById(R.id.cardChoice2);
+        double basePrice = currentProduct.getPrice();
+        double baseOldPrice = currentProduct.getCompareAtPrice();
+        cardChoice1.setOnClickListener(v -> {
+            cardChoice1.setBackgroundResource(R.color.orange_primary);
+            cardChoice1.setTextColor(androidx.core.content.ContextCompat.getColor(this, R.color.white));
+            cardChoice2.setBackgroundResource(android.R.color.transparent);
+            cardChoice2.setTextColor(androidx.core.content.ContextCompat.getColor(this, R.color.dark_gray));
+            updatePriceDisplay(basePrice, baseOldPrice);
+        });
+        cardChoice2.setOnClickListener(v -> {
+            cardChoice2.setBackgroundResource(R.color.orange_primary);
+            cardChoice2.setTextColor(androidx.core.content.ContextCompat.getColor(this, R.color.white));
+            cardChoice1.setBackgroundResource(android.R.color.transparent);
+            cardChoice1.setTextColor(androidx.core.content.ContextCompat.getColor(this, R.color.dark_gray));
+            updatePriceDisplay(basePrice + 500000, baseOldPrice + 500000);
+        });
+
+    }
+    private void updatePriceDisplay(double newPrice, double oldPrice) {
+        java.text.DecimalFormat formatter = new java.text.DecimalFormat("#,###");
+        android.widget.TextView tvPrice = findViewById(R.id.tvPriceNew);
+        tvPrice.setText(String.format(java.util.Locale.US, "%,.0f VNĐ", newPrice));
+        android.widget.TextView tvOldPrice = findViewById(R.id.tvPriceOld);
+        tvPrice.setText(String.format(java.util.Locale.US, "%,.0f VNĐ", oldPrice));
     }
 }
