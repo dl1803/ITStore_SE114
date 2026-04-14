ae466b01a1f22dcd7f252d9550e47ebf31453c2f
dl1803 <loitran0990@gmail.com>
2026-04-14 14:36:11 +0700

    feat : Them tinh nang xu li (khi them/xoa san pham da chon) gia toi thieu khi chon voucher trong gio hang

--- app/src/main/java/com/example/itstore/model/Discount.java
+++ app/src/main/java/com/example/itstore/model/Discount.java
@@ -6,17 +6,20 @@ public class Discount {
     private String condition;
     private String date;
     private double amount;
+    private double minOrderValue;
 
-    public Discount(String code, String title, String condition, String date, double amount) {
+    public Discount(String code, String title, String condition, String date, double amount, double minOrderValue) {
         this.code = code;
         this.title = title;
         this.condition = condition;
         this.date = date;
         this.amount = amount;
+        this.minOrderValue = minOrderValue;
     }
     public String getCode() { return code; }
     public String getTitle() { return title; }
     public String getCondition() { return condition; }
     public String getDate() { return date; }
     public double getAmount() { return amount; }
+    public double getMinOrderValue() { return minOrderValue; }
 }
