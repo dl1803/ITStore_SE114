ff2687951e9d9e799f07fcc4929c98c3d1703141
vuongtapcode <24522050@gm.uit.edu.vn>
2026-04-24 17:10:50 +0700

    feat: Hoan thanh chuc nang xem cau hinh chi tiet o ProductDetailActivity

--- app/src/main/java/com/example/itstore/fragment/SpecsBottomSheet.java
+++ app/src/main/java/com/example/itstore/fragment/SpecsBottomSheet.java
@@ -4,45 +4,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.example.itstore.R;
+import com.example.itstore.adapter.SpecAdapter;
+import com.example.itstore.databinding.DialogProductSpecsBinding;
 import com.example.itstore.model.Product;
+import com.example.itstore.model.Specification;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpecsBottomSheet extends BottomSheetDialogFragment {
 
-    private Product product;
-
+    private Product currentProduct;
+    private DialogProductSpecsBinding binding;
     public SpecsBottomSheet(Product product) {
-        this.product = product;
+        this.currentProduct = product;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_bottom_sheet_specs, container, false);
+        binding = DialogProductSpecsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView tvDescription = view.findViewById(R.id.tvDescription);
-        if (product != null) {
-            String desc = product.getDescription();
-            if (desc != null && !desc.isEmpty()) {
-                tvDescription.setText(desc);
-            } else {
-                tvDescription.setText("Đang cập nhật thông tin cấu hình...");
-            }
+        List<Specification> specs = new ArrayList<>();
+        specs.add(new Specification("Thương hiệu", "Kingston"));
+        specs.add(new Specification("Dung lượng", "16GB (1x16GB)"));
+        specs.add(new Specification("Loại RAM", "DDR4 Desktop"));
+        specs.add(new Specification("Bus RAM", "3200MHz"));
+        specs.add(new Specification("Độ trễ (Cas)", "CL16"));
+        specs.add(new Specification("Điện áp", "1.35V"));
+        specs.add(new Specification("Tản nhiệt", "Nhôm đen"));
+
+        //Setup RecyclerView
+        SpecAdapter specAdapter = new SpecAdapter(specs);
+        binding.rvProductSpecs.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.rvProductSpecs.setAdapter(specAdapter);
+
+        if (specs.size() > 5) {
+            binding.lineDivider.setVisibility(View.VISIBLE);
+            binding.btnToggleSpecs.setVisibility(View.VISIBLE);
+        } else {
+            binding.lineDivider.setVisibility(View.GONE);
+            binding.btnToggleSpecs.setVisibility(View.GONE);
         }
+
+        // Bắt sự kiện bấm nút xem tất cả hoặc thu vào
+        binding.btnToggleSpecs.setOnClickListener(v -> {
+            boolean currentExpanded = specAdapter.isExpanded();
+            specAdapter.setExpanded(!currentExpanded);
+            if (specAdapter.isExpanded()) {
+                binding.btnToggleSpecs.setText("Thu gọn ˄");
+            } else {
+                binding.btnToggleSpecs.setText("Xem tất cả ˅");
+            }
+        });
     }
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
